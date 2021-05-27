72531c638834ca9fe85f16e8372f32aefe32fd7c
shivambansals <shivam.bansal_iot18@gla.ac.in>
2021-05-27 19:43:22 +0530

    whatsapp message sent

--- whatsappMsg.js
+++ whatsappMsg.js
@@ -1,19 +1,19 @@
 const puppeteer = require("puppeteer");
-async function scrape(url) {
-    const browser = await puppeteer.launch({ headless: false });
+(async function () {
+    const browser = await puppeteer.launch({ headless: false ,defaultViewport : null , args: ["--start-maximized"] });
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto("https://web.whatsapp.com/");
     await page.waitForSelector('span[data-testid="search"]', { visible: true });
     await page.click('span[data-testid="search"]')
-    await page.type("._2_1wd.copyable-text.selectable-text", "name of the contact");//put name of the contact
+    await page.type("._2_1wd.copyable-text.selectable-text", "contact name");//put name of the contact
     await page.keyboard.press("Enter");
     await page.waitForSelector('div[data-tab="6"]', { visible: true })
     const inp = await page.$('div[data-tab="6"]');
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 2; i++) {
         await inp.type("magic")
         await page.keyboard.press("Enter")
     }
+    console.log("message sent!!")
 
 
-}
-scrape("https://web.whatsapp.com/")
+})();
