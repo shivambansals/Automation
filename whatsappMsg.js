2db97a8ce7289a396a336fd0464d77c5a714261a
shivambansals <shivam.bansal_iot18@gla.ac.in>
2021-05-27 18:55:33 +0530

    whatsapp automation done

---
+++ whatsappMsg.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+async function scrape(url) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('span[data-testid="search"]', { visible: true });
+    await page.click('span[data-testid="search"]')
+    await page.type("._2_1wd.copyable-text.selectable-text", "name of the contact");//put name of the contact
+    await page.keyboard.press("Enter");
+    // await page.waitForSelector('span [title="My Jio"]')
+    // const target = await page.$('span [title="My Jio"]')
+    //await target.click('span [title="8077179601"]')
+    await page.waitForSelector('div[data-tab="6"]', { visible: true })
+    const inp = await page.$('div[data-tab="6"]');
+    for (let i = 0; i < 10; i++) {
+        await inp.type("magic")
+        await page.keyboard.press("Enter")
+    }
+
+
+}
+scrape("https://web.whatsapp.com/")
